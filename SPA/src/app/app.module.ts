ca96b9e5e77a815b2d27cfe926924ea0de8f87bd
NemPej <59743507+NemPej@users.noreply.github.com>
2021-03-29 10:43:26 +0200

    bootstrapNgxAdded

--- SPA/src/app/app.module.ts
+++ SPA/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
@@ -26,8 +28,10 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
    ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [AuthService, ErrorInterceptorProvider],
   bootstrap: [AppComponent]

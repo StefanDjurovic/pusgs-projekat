7cf34a79625c9620bd936cb1a0831ebefaaafdbf
stefan <stefandjurovic1998@gmail.com>
2021-06-13 08:45:34 +0200

    notification and image work

---
+++ API/Models/AddressPriority.cs
@@ -0,0 +1,10 @@
+namespace API.Models
+{
+    public class AddressPriority
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int Priority { get; set; }
+    }
+}
